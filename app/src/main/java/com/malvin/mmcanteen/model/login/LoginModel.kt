37860362b8fecc6902db4b546b2d3c84cb3f05c5
dvlwj <dvlimweijiang@gmail.com>
2018-12-10 16:58:03 +0700

    update

--- app/src/main/java/com/malvin/mmcanteen/model/login/LoginModel.kt
+++ app/src/main/java/com/malvin/mmcanteen/model/login/LoginModel.kt
@@ -4,13 +4,14 @@ class LoginModel {
     interface OnLoginFinishedListener {
         fun onUsernameError()
         fun onPasswordError()
-        fun onSuccess()
+        fun onSuccess(username: String,password: String)
     }
-    fun login(username: String, password: String, listener: OnLoginFinishedListener){
+
+    fun login(username: String, password: String, listener: OnLoginFinishedListener) {
         when {
             username.isEmpty() -> listener.onUsernameError()
             password.isEmpty() -> listener.onPasswordError()
-            else -> listener.onSuccess()
+            else -> listener.onSuccess(username,password)
         }
     }
 }
--- app/src/main/java/com/malvin/mmcanteen/presenter/login/LoginPresenter.kt
+++ app/src/main/java/com/malvin/mmcanteen/presenter/login/LoginPresenter.kt
@@ -1,30 +1,40 @@
 package com.malvin.mmcanteen.presenter.login
 
+import com.github.kittinunf.fuel.Fuel
 import com.malvin.mmcanteen.model.login.LoginModel
+import com.malvin.mmcanteen.utility.ServerAddress
+import com.malvin.mmcanteen.utility.SessionManagement
 import com.malvin.mmcanteen.view.login.LoginView
 
-class LoginPresenter(var loginView: LoginView?, var loginModel: LoginModel ) : LoginModel.OnLoginFinishedListener {
+class LoginPresenter(var loginView: LoginView?, var loginModel: LoginModel) : LoginModel.OnLoginFinishedListener {
 
-    fun validateCredentials(username: String, password: String){
+    fun validateCredentials(username: String, password: String) {
         loginView?.showProgress()
-        loginModel.login(username,password,this)
+        loginModel.login(username, password)
     }
+
     fun onDestroy() {
         loginView = null
     }
+
     override fun onUsernameError() {
         loginView?.apply {
             setUsernameError()
             hideProgress()
         }
     }
+
     override fun onPasswordError() {
         loginView?.apply {
             setUsernameError()
             hideProgress()
         }
     }
-    override fun onSuccess() {
+
+    override fun onSuccess(username: String,password: String) {
+        val data = listOf("name" to username,"password" to password)
+        val serverAddress = SessionManagement.
+        Fuel.post(ServerAddress.)
         loginView?.navigateToHome()
     }
 }
