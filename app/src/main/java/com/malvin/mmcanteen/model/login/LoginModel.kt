fd470c04b95e2f0e9d3cfec880205655c687a7b8
dvlwj <dvlimweijiang@gmail.com>
2018-12-13 16:36:10 +0700

    update

--- app/src/main/java/com/malvin/mmcanteen/model/login/LoginModel.kt
+++ app/src/main/java/com/malvin/mmcanteen/model/login/LoginModel.kt
@@ -1,17 +1,40 @@
 package com.malvin.mmcanteen.model.login
 
-class LoginModel {
-    interface OnLoginFinishedListener {
-        fun onUsernameError()
-        fun onPasswordError()
-        fun onSuccess(username: String,password: String)
-    }
+import java.util.*
 
-    fun login(username: String, password: String, listener: OnLoginFinishedListener) {
-        when {
-            username.isEmpty() -> listener.onUsernameError()
-            password.isEmpty() -> listener.onPasswordError()
-            else -> listener.onSuccess(username,password)
+//class LoginModel {
+//    interface OnLoginFinishedListener {
+//        fun onUsernameError()
+//        fun onPasswordError()
+//        fun onSuccess(username: String,password: String)
+//    }
+//
+//    fun login(username: String, password: String, listener: OnLoginFinishedListener) {
+//        when {
+//            username.isEmpty() -> listener.onUsernameError()
+//            password.isEmpty() -> listener.onPasswordError()
+//            else -> listener.onSuccess(username,password)
+//        }
+//    }
+//}
+class LoginModel: Observable(){
+    /// The first name of the user
+    var username: String = ""
+        set(value) {
+            field = value
+            setChangedAndNotify("username")
         }
+
+    /// The last name of the user
+    var password: String = ""
+        set(value) {
+            field = value
+            setChangedAndNotify("password")
+        }
+
+    private fun setChangedAndNotify(field: Any)
+    {
+        setChanged()
+        notifyObservers(field)
     }
 }
